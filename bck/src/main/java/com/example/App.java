320f52a189e05df96a51bdaaca7933002d489eba
beekaa6 <juniorbeka63k@gmail.com>
2024-11-13 16:19:32 +0100

    Add spring boot dependencies to backend folder

--- bck/src/main/java/com/example/App.java
+++ bck/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    System.out.print("test");
+    SpringApplication.run(App.class, args);
   }
 }
