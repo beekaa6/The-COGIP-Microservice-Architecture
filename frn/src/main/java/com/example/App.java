2b4b30f9e7283802805cca951ca48c5b5b38537b
beka <juniorbeka63k@gmail.com>
2024-11-14 12:28:31 +0100

    Add cli to frontend, with get request

--- frn/src/main/java/com/example/App.java
+++ frn/src/main/java/com/example/App.java
@@ -1,7 +1,47 @@
 package com.example;
 
-public class App {
+import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class App implements CommandLineRunner{
+  private final RestTemplate restTemplate = new RestTemplate();
+  private final Scanner scanner = new Scanner(System.in);
+  private String url = "http://localhost:8080/companies";
+
   public static void main(String[] args) {
-    System.out.print("test");
+    SpringApplication.run(App.class, args);
+  }
+
+  @Override
+  public void run(String... args){
+    while (true) {
+      System.out.print("Command: ");
+      String input = scanner.nextLine().toLowerCase().trim();
+      if (input.equals("quit") || input.isEmpty()) {
+        System.out.println("Quitting");
+        break;
+      }
+      if (input.equals("list")) {
+        System.out.println("List: "+getAllList());
+      }
+    }
+  }
+
+  public String getAllList(){
+    ResponseEntity<String> response = restTemplate.exchange(
+      url, 
+      HttpMethod.GET,
+      null,
+      new ParameterizedTypeReference<String>() {}
+    );
+    return response.getBody();
   }
 }
