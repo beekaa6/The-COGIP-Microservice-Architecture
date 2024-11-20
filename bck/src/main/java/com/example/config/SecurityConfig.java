31a6151d4f7775c6601580ac6f4efcdd3e40ef08
beekaa6 <juniorbeka63k@gmail.com>
2024-11-20 11:59:44 +0100

    Add users with roles (in memory user details)

--- bck/src/main/java/com/example/config/SecurityConfig.java
+++ bck/src/main/java/com/example/config/SecurityConfig.java
@@ -2,8 +2,16 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +20,41 @@ public class SecurityConfig {
   
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf(customizer -> customizer.disable())
+    return http
+              .csrf(customizer -> customizer.disable())
+              .authorizeHttpRequests(request -> request
+                  .requestMatchers("/companies/**").hasRole("ADMIN")
+                  .requestMatchers("/contact/**").hasAnyRole("ADMIN", "ACCOUNTANT", "INTERN")
+                  .anyRequest().authenticated())
+              //.formLogin(Customizer.withDefaults())
+              .httpBasic(Customizer.withDefaults())
+              .sessionManagement(session -> 
+                  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
               .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+
+    UserDetails beka = User.withUsername("beka")
+        .password(passwordEncoder.encode("test"))
+        .roles("ADMIN")
+        .build();
+
+    UserDetails kasoy = User.withUsername("kasoy")
+        .password(passwordEncoder.encode("test"))
+        .roles("ACCOUNTANT")
+        .build();
+    
+    UserDetails user = User.withUsername("user")
+        .password(passwordEncoder.encode("test"))
+        .roles("INTERN")
+        .build();
+    return new InMemoryUserDetailsManager(beka, kasoy, user);
   }
 }
