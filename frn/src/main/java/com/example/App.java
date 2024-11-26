23a09aa58053afe43a6b7f7ffb37acee2424845f
beka <juniorbeka63k@gmail.com>
2024-11-26 15:39:18 +0100

    Add get list request for CLI

--- frn/src/main/java/com/example/App.java
+++ frn/src/main/java/com/example/App.java
@@ -4,19 +4,26 @@ public class App {
   public static void main(String[] args) {
     if (args.length == 0) {
       System.out.println("No args");
+    } else if (args[0].equals("list")) {
+      list(args);
+    } else if (args[0].equals("delete")) {
+      delete(args);
     } else if (args[0].equals("add")) {
       add(args);
-    } else if (args[0].equals("delete")) {
-      add(args);
-    } else if (args[0].equals("list")) {
-      add(args);
+    } else {
+      System.out.println("Unknown command: "+args[0]);
     }
+  }
 
+  public static void list(String args[]){
+    Requests requests = new Requests();
+    requests.getList();
   }
 
   public static void add(String args[]){
     if (args.length < 2) {
       System.out.println("Missing required arguments");
+      System.out.println("Example: add company --name=<NAME> --vat=<VAT>");
     }  else {
       if (args[1].equals("company")) {
         String name = null;
@@ -34,6 +41,7 @@ public class App {
           System.out.println("Adding company: ");
           System.out.println("Company Name: "+name);
           System.out.println("Company Vat: "+vat);
+          //tba
         }
       } else {
         System.out.println("Unknown Arguments");
@@ -43,9 +51,6 @@ public class App {
 
   public static void delete(String args[]){
     System.out.println(args[0]);
-  }
-
-  public static void list(String args[]){
-    System.out.println(args[0]);
+    //tba
   }
 }
---
+++ frn/src/main/java/com/example/Requests.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Requests {
+  private String url = "https://jsonplaceholder.typicode.com/posts/1";
+  private HttpClient client = HttpClient.newHttpClient();
+
+  public void getList(){
+    try {
+      HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+
+      HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+      
+      System.out.println("Response Status: "+response.statusCode());
+      System.out.println("Response Body: ");
+      System.out.println(response.body());
+    } catch (Exception e) {
+      System.out.println("Error: "+e.getMessage());
+    }
+  }
+
+}
